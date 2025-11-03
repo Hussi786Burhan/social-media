1a65ac4d8d801972bcd44bcaf5871ad85b1c4919
Yasmeen Merchant <yasmeenburhan78653@gmail.com>
2025-11-03 21:14:13 +0530

    Added Loading

--- social-media/src/components/PostList.jsx
+++ social-media/src/components/PostList.jsx
@@ -1,29 +1,52 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/Post-list-store";
 import Message from "./Message";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
 
+  const [fetching, setfetching] = useState(false);
+
   useEffect(() => {
-    fetch("https://dummyjson.com/posts")
-      .then((res) => res.json())
-      .then((data) => {
-        addInitialPosts(data.posts);
-      });
-  }, [addInitialPosts]);
+  setfetching(true);
+  const controller = new AbortController();
+  const signal = controller.signal;
+
+  fetch("https://dummyjson.com/posts", { signal })
+    .then((res) => res.json())
+    .then((data) => {
+      addInitialPosts(data.posts);
+      setfetching(false);
+    })
+    .catch((err) => {
+      if (err.name === "AbortError") {
+        console.log("Fetch aborted");
+      } else {
+        console.error(err);
+      }
+    });
+
+  return () => {
+    console.log("Cleaning up useEffect");
+    controller.abort();
+  };
+}, []);
+
 
   return (
     <>
-      {postList.length === 0 && <Message />}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {fetching && <LoadingSpinner />}
+
+      {!fetching && postList.length === 0 && <Message />}
+
+      {!fetching && postList.length > 0 &&
+        postList.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
     </>
   );
 };
 
 export default PostList;
-
-
